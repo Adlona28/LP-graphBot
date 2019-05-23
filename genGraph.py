9ecb8488f5f4f63c92943883631607b382c6ce22
adlona28 <adrian.lozano.navarro@est.fib.upc.edu>
2019-05-23 15:10:56 +0200

    update

--- genGraph.py
+++ genGraph.py
@@ -1,10 +1,10 @@
 import csv
-import pandas as pd
-url = 'https://github.com/jordi-petit/lp-graphbot-2019/blob/master/dades/worldcitiespop.csv.gz?raw=true'
-cityName = 'City'
-lon = 'Longitude'
-lat = 'Latitude'
-population = 'Population'
-df = pd.read_csv(url, usecols=[cityName, lon, lat, population])
-for row in df.itertuples():
-    
+with open('./worldcities.csv') as csvfile:
+	readCSV = csv.reader(csvfile, delimiter=',')
+	with open('worldCitiesWithPopulation.csv', 'w') as newFile:
+	    writer = csv.writer(newFile)
+	    for row in readCSV:
+	    	if row[4] != "":
+	    		writer.writerow([row[0], row[1], row[4], row[5], row[6]])
+	newFile.close()
+csvfile.close()
