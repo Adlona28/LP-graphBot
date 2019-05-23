5c1bb207ba506e352e68fc2bf9086ebcb9ac535e
adlona28 <adrian.lozano.navarro@est.fib.upc.edu>
2019-05-23 22:14:44 +0200

    fixed difference of the list size and graph size

--- genGraph.py
+++ genGraph.py
@@ -12,8 +12,8 @@ def normalizedLong(longitud):
 
 def coordLimit(distance, latitude, longitud):
     kmPerGrau = haversine((latitude, 0.0), (latitude, 1.0))
-    kmPerGrau = kmPerGrau + 5
-    return normalizedLong(longitud + int(distance/kmPerGrau))
+    kmPerGrau = kmPerGrau
+    return normalizedLong(longitud + int(distance/kmPerGrau) + 1)
 
 
 def getKey(item):
@@ -22,8 +22,11 @@ def getKey(item):
 
 def graph(N, M):
     G = nx.Graph()
+    countNodes = 0
+    c = 0
     for node in N:
-        G.add_node(node[1], country=node[0], lat=node[2], long=node[3])
+        G.add_node(node[4], country=node[0], city=node[1],
+                   lat=node[2], long=node[3])
     G.add_edges_from(M)
     return G
 
@@ -32,9 +35,11 @@ def readFile(population):
     L = []
     with open('./worldCitiesWithPopulation.csv') as file:
         readCSV = csv.reader(file, delimiter=',')
+        id = 1
         for row in readCSV:
             if float(row[2]) >= population:
-                L.append([row[0], row[1], float(row[3]), float(row[4])])
+                L.append([row[0], row[1], float(row[3]), float(row[4]), id])
+                id = id+1
     file.close()
     L.sort(key=getKey)
     return L
@@ -46,22 +51,33 @@ def genEdges(L, distance):
     while i < len(L):
         longLimit = coordLimit(distance, L[i][2], L[i][3])
         longActual = L[i][3]
+        bandera = False
+        if longActual > longLimit:
+            bandera = True
         j = i
-        while longActual <= longLimit and j < len(L):
+        while bandera or longActual <= longLimit:
             if haversine((L[i][2], L[i][3]), (L[j][2], L[j][3])) <= distance:
-                A.append([L[i][1], L[j][1]])
+                A.append([L[i][4], L[j][4]])
             longActual = L[j][3]
             j = j + 1
+            if j == len(L):
+                if bandera:
+                    j = 0
+                    bandera = False
+                else:
+                    longActual = 100000.0
         i = i+1
-    print(len(L))
-    print(len(A))
     return A
 
 
 def genGraph(population, distance):
     L = readFile(population)
     A = genEdges(L, distance)
+    print(len(L))
+    print(len(A))
     return graph(L, A)
 
 
-genGraph(100000, 300)
+G = genGraph(100000, 300)
+print(G.number_of_nodes())
+print(G.number_of_edges())
