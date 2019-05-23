1f04d838193ea1f9bf34bb5e2a4b00ae816f23e2
adlona28 <adrian.lozano.navarro@est.fib.upc.edu>
2019-05-23 21:21:43 +0200

    generacion del grafo done

--- genGraph.py
+++ genGraph.py
@@ -1,10 +1,67 @@
 import csv
-with open('./worldcities.csv') as csvfile:
-	readCSV = csv.reader(csvfile, delimiter=',')
-	with open('worldCitiesWithPopulation.csv', 'w') as newFile:
-	    writer = csv.writer(newFile)
-	    for row in readCSV:
-	    	if row[4] != "":
-	    		writer.writerow([row[0], row[1], row[4], row[5], row[6]])
-	newFile.close()
-csvfile.close()
+import networkx as nx
+from haversine import haversine
+
+
+def normalizedLong(longitud):
+    longitud = longitud + 180.0
+    longitud = longitud % 360.0
+    longitud = longitud - 180.0
+    return longitud
+
+
+def coordLimit(distance, latitude, longitud):
+    kmPerGrau = haversine((latitude, 0.0), (latitude, 1.0))
+    kmPerGrau = kmPerGrau + 5
+    return normalizedLong(longitud + int(distance/kmPerGrau))
+
+
+def getKey(item):
+    return item[3]
+
+
+def graph(N, M):
+    G = nx.Graph()
+    for node in N:
+        G.add_node(node[1], country=node[0], lat=node[2], long=node[3])
+    G.add_edges_from(M)
+    return G
+
+
+def readFile(population):
+    L = []
+    with open('./worldCitiesWithPopulation.csv') as file:
+        readCSV = csv.reader(file, delimiter=',')
+        for row in readCSV:
+            if float(row[2]) >= population:
+                L.append([row[0], row[1], float(row[3]), float(row[4])])
+    file.close()
+    L.sort(key=getKey)
+    return L
+
+
+def genEdges(L, distance):
+    A = []
+    i = 0
+    while i < len(L):
+        longLimit = coordLimit(distance, L[i][2], L[i][3])
+        longActual = L[i][3]
+        j = i
+        while longActual <= longLimit and j < len(L):
+            if haversine((L[i][2], L[i][3]), (L[j][2], L[j][3])) <= distance:
+                A.append([L[i][1], L[j][1]])
+            longActual = L[j][3]
+            j = j + 1
+        i = i+1
+    print(len(L))
+    print(len(A))
+    return A
+
+
+def genGraph(population, distance):
+    L = readFile(population)
+    A = genEdges(L, distance)
+    return graph(L, A)
+
+
+genGraph(100000, 300)
